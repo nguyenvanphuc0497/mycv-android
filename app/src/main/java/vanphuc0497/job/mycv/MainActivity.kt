e3dd64aebc2e66ddf144d5b9434e7004415f3538
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2020-02-10 11:15:24 +0700

    init project

---
+++ app/src/main/java/vanphuc0497/job/mycv/MainActivity.kt
@@ -0,0 +1,12 @@
+package vanphuc0497.job.mycv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
