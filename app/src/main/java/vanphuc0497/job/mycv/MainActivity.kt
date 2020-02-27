3adb07000c25f7b057876dd4a785e9a62c034e56
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2020-02-27 15:35:34 +0700

    implement libary firebase

--- app/src/main/java/vanphuc0497/job/mycv/MainActivity.kt
+++ app/src/main/java/vanphuc0497/job/mycv/MainActivity.kt
@@ -2,11 +2,17 @@ package vanphuc0497.job.mycv
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvTest.setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
     }
 }
